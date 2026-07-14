ed2eecc377f38e4cfd0ff26fe3c0e9448a1b285b
JaredBurnham-tempsites <jared.burnham@icloud.com>
2026-07-14 06:06:05 -0700

    feer the man who has practiced one punch a thousand time this is my first punch lol

---
+++ createvariables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int num = 1;
+	float myrootbeerfloat = 5007.8332;
+	char mycharacter = 'R';
+
+	printf("%d\n%f\n%c\n", num, myrootbeerfloat, mycharacter);
+	return 0;
+}
---
+++ escapeCharacters.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf("ima escape some characters XD \\ \n");
+	return 0;
+}
