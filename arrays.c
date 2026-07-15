0af874efe0d32e24cfd55d87485fce8699dd32f7
JaredBurnham-tempsites <jared.burnham@icloud.com>
2026-07-15 04:52:26 -0700

    more arrays with loppty loops and maybe even a cartwheel nah we never reset a loop but we sure do know how to make an array loopty loop fall on its head XD

--- arrays.c
+++ arrays.c
@@ -6,4 +6,23 @@ int main(){
 	for(i = 0; i <= 8; i++){
 		printf("the number %d\n", allTheNumbers[i]);
 	}
+	allTheNumbers[3] = 33;
+	printf("4 becomes %d mwahaha\n",allTheNumbers[3]);
+	int sizedArray[4] = {10,20,30,40};// sized array important for known list size and saving memory
+
+	for(i = 0; i < 4; i++){
+	printf("the sized array is speaaaakkkiiing %d\n", sizedArray[i]);
+	}
+
+	printf("%zu is how big allTheNumbers do be\n", sizeof(allTheNumbers));
+	int length = sizeof(allTheNumbers) / sizeof(allTheNumbers[0]);
+
+	printf("actual length of allTheNumbers is %d", length);
+
+	//better loop here we goooooo
+	for(i = 0; i < length; i++){
+	printf("%d\n", allTheNumbers[i]);
+	}
+	// toodoo multidimensional array maybe even 5 levels deep delving into the multiverse with that one eh
+
 }
