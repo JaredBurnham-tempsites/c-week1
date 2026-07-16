8ed00b2e7595a3a2237570d4f97e6673ee443f50
JaredBurnham-tempsites <jared.burnham@icloud.com>
2026-07-16 03:55:35 -0700

    hey look functions are declared and then described this makes sense not for all them h files i done made without understanding oof also data to data after processing wooty hoot hoot

--- functionfun.c
+++ functionfun.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 
-void myfunction(){
-	printf("hello from my first c function");
-}
+void myfunction();
+void argumentative(char letsgo[]);
+int ineeddata(int nums[2]);
+int gotdata(int num);
 
 int main(){
 	myfunction();
+
+	argumentative("hello");
+	int b[2] = {1,2};
+	int a = ineeddata(b);
+	printf("data receied %d",gotdata(a));
 	return 0;
 }
+
+
+void myfunction(){
+	printf("hello from my first c function\n");
+}
+
+void argumentative(char letsgo[]){
+	printf("%s\nIs what you say\n NOOOOO \n",letsgo);
+}
+
+int ineeddata(int nums[2]){
+	int r =nums[0] + nums[1];
+	return r;
+}
+
+int gotdata(int num){
+	return num;
+}
+
