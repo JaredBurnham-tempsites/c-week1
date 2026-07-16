5d234d26390e3b2686aa7f691c2a9a50cfe40af3
JaredBurnham-tempsites <jared.burnham@icloud.com>
2026-07-15 19:55:54 -0700

    now I can read your mind through your fingertips become my slaves you peons

---
+++ userinput.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	int usernumber;
+	char name[20];
+	printf("type a number user user\n");
+	scanf("%d", &usernumber);
+
+	printf("%d is yur number wooo im magic\n", usernumber);
+
+	scanf("%s", name);
+	printf("you enter %s\n",name);
+}
