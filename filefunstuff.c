e6c02ea8b93498834739b540353845a0045b89f4
JaredBurnham-tempsites <jared.burnham@icloud.com>
2026-07-17 03:34:07 -0700

    hey the current secrets of the inuverse have been written but are they the real secrets idk

--- filefunstuff.c
+++ filefunstuff.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
-//dark energy is just dah blood flow XD dark matter is just gray matter just remember up and down down and up in and out out and in
 
 FILE *fptr; //working with files require pointers FILE is a data type
 
 int main(){
-	fptr = fopen("secretsoftheuniverse.txt", "w");
-	
+	char files[25] = "secretsoftheuniverse.txt";
+	fptr = fopen(files, "r");
+	if(fptr == NULL){printf("not able to open file");}
+	else{
+		char content[100];
+		while(fgets(content, 100, fptr)){
+			printf("%s",content);
+		}
+		fclose(fptr);
+	}
+
+	fptr = fopen(files, "w");
+	fprintf(fptr,"dark energy is just dah blood flow XD dark matter is just gray matter just remember up and down down and up in and out out and in");
+	fclose(fptr);
+	fptr = fopen(files, "a");
+	fprintf(fptr, "\n hey we just added instead of overwritting yay");
 	fclose(fptr);
 }
 
