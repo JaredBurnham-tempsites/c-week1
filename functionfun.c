f8ff4e51010fd12763dc89a04796879e1e0015ae
JaredBurnham-tempsites <jared.burnham@icloud.com>
2026-07-17 03:09:41 -0700

    got the basics of functions probably skipped a few less than I shoulda have bouta make a lot of functions with all these millions of idears i dun goot

--- functionfun.c
+++ functionfun.c
@@ -2,16 +2,30 @@
 
 void myfunction();
 void argumentative(char letsgo[]);
+void s();
+void c();
+void coffee(void(*func)());
 int ineeddata(int nums[2]);
 int gotdata(int num);
 
 int main(){
+	void (*cofees[2])() = {c,s};
 	myfunction();
-
+	int (*fptr)(int) = gotdata;
+	printf("using pointer as function %d\n", fptr(10));
 	argumentative("hello\n");
 	int b[2] = {1,2};
 	int a = ineeddata(b);
 	printf("data receied %d\n",gotdata(a));
+
+	printf("passing functions\n\n");
+	coffee(s);
+	coffee(c);
+	printf("function array");
+	int i;
+	for(i = 0; i < 2; i++){
+		cofees[i]();
+	}
 	return 0;
 }
 
@@ -33,3 +47,14 @@ int gotdata(int num){
 	return num;
 }
 
+void coffee(void (*func)()){
+	func();
+}
+
+void s(){
+	printf("starbucks\n");
+}
+
+void c(){
+	printf("columbian\n");
+}
