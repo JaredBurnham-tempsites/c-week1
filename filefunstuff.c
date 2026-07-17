7e1669b20d544afca215490d87e0e81a5ebb9074
JaredBurnham-tempsites <jared.burnham@icloud.com>
2026-07-17 03:16:12 -0700

    opening the file to the secrets of the universe

--- filefunstuff.c
+++ filefunstuff.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 
+//dark energy is just dah blood flow XD dark matter is just gray matter just remember up and down down and up in and out out and in
+
+FILE *fptr; //working with files require pointers FILE is a data type
+
+int main(){
+	fptr = fopen("secretsoftheuniverse.txt", "w");
+	
+	fclose(fptr);
+}
+
 
