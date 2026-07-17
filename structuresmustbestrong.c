221145d91dbcd207289178e5cc1003222e5cfec8
JaredBurnham-tempsites <jared.burnham@icloud.com>
2026-07-17 04:52:28 -0700

    structures built by the structure machine viahuman hands

---
+++ structuresmustbestrong.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct stronk {
+	int sint;
+	char schar;
+};
+
+void printer(struct stronk *s){
+	printf("heres my stronk stats %d %c\n",s->sint, s->schar);
+}
+
+int main(){
+	struct stronk s1;
+	s1.sint = 275056;
+	s1.schar = 'S';
+
+	struct stronk s2;
+	s2.sint = 1;
+	s2.schar = 'c';
+
+	struct stronk *sptr = &s1;
+
+	printf("stronk one number %d,and character %c\n", sptr->sint, sptr->schar);
+	printf("stronk 2 number and char %d %c\n", s2.sint, s2.schar);
+
+	printer(&s1);
+	printer(&s2);
+
+	return 0;
+}
